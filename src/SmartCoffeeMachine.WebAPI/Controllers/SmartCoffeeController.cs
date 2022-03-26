73b78cde159b8b5e290ef2db20f774f1e25dce49
Shawn Shi <ss.shawnshi@gmail.com>
2022-03-26 15:29:44 -0400

    Add Smart Coffee controller.

---
+++ src/SmartCoffeeMachine.WebAPI/Controllers/SmartCoffeeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SmartCoffeeMachine.WebAPI.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace SmartCoffeeMachine.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SmartCoffeeController : ControllerBase
+    {
+        
+        private readonly ILogger<SmartCoffeeController> _logger;
+
+        public SmartCoffeeController(ILogger<SmartCoffeeController> logger)
+        {
+            _logger = logger;
+        }
+
+        // POST: api/SmartCoffee
+        /// <summary>
+        ///     Request to make a new coffee 
+        /// </summary>
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateCoffeeModel createCoffeeModel)
+        {
+            Guid orderId = Guid.NewGuid();
+
+            // TODO : decouple long-running task using async message queue.
+
+            return Ok(orderId);
+        }
+
+    }
+}
