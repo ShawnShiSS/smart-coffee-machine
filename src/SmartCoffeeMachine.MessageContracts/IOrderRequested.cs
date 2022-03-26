5967354c4d72c13fbc148bec4beceff923170f2e
Shawn Shi <ss.shawnshi@gmail.com>
2022-03-26 15:27:06 -0400

    Class library for message contracts.

---
+++ src/SmartCoffeeMachine.MessageContracts/IOrderRequested.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartCoffeeMachine.MessageContracts
+{
+    public interface IOrderRequested
+    {
+        /// <summary>
+        /// Order Id.
+        /// </summary>
+        Guid OrderId { get; }
+
+        /// <summary>
+        /// Coffee type.
+        /// </summary>
+        string Type { get; }
+    }
+
+}
