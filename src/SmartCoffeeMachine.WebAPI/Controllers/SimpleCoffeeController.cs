e43a122d6f63194133a32f7682f851114fc7fc92
Shawn Shi <ss.shawnshi@gmail.com>
2022-03-26 15:13:14 -0400

    API: add simple coffee API endpoint to just demonstrate timeout 408.

---
+++ src/SmartCoffeeMachine.WebAPI/Controllers/SimpleCoffeeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SmartCoffeeMachine.WebAPI.Models;
+using System.Threading.Tasks;
+
+namespace SmartCoffeeMachine.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SimpleCoffeeController : ControllerBase
+    {
+        
+        private readonly ILogger<SimpleCoffeeController> _logger;
+
+        public SimpleCoffeeController(ILogger<SimpleCoffeeController> logger)
+        {
+            _logger = logger;
+        }
+
+        // POST: api/SimpleCoffee
+        /// <summary>
+        ///     Request to make a new coffee 
+        /// </summary>
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateCoffeeModel createCoffeeModel)
+        {
+            await Task.Delay(3000);
+
+            return StatusCode(StatusCodes.Status408RequestTimeout);
+        }
+
+    }
+}
---
+++ src/SmartCoffeeMachine.WebAPI/Models/CreateCoffeeModel.cs
@@ -0,0 +1,14 @@
+﻿namespace SmartCoffeeMachine.WebAPI.Models
+{
+    /// <summary>
+    ///     API model to create a new coffee
+    /// </summary>
+    public class CreateCoffeeModel
+    {
+        /// <summary>
+        ///     Type of coffee. I.e., French Press.
+        /// </summary>
+        public string Type { get; set; }
+
+    }
+}
