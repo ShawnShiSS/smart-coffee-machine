2e2c7466474b47a4ac30c465e5614e0d56d726c7
Shawn Shi <ss.shawnshi@gmail.com>
2022-03-26 20:11:20 -0400

    Add Message components library and initial consumer.

---
+++ src/SmartCoffeeMachine.MessageComponents/Consumers/MakeCoffeeConsumer.cs
@@ -0,0 +1,32 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Logging;
+using SmartCoffeeMachine.MessageContracts;
+using System;
+using System.Threading.Tasks;
+
+namespace SmartCoffeeMachine.MessageComponents.Consumers
+{
+
+    /// <summary>
+    ///     Consumer to actually make a coffee.
+    /// </summary>
+    public class MakeCoffeeConsumer : IConsumer<IOrderAccepted>
+    {
+        private readonly ILogger<MakeCoffeeConsumer> _logger;
+
+        public MakeCoffeeConsumer(ILogger<MakeCoffeeConsumer> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task Consume(ConsumeContext<IOrderAccepted> context)
+        {
+            _logger.LogInformation($"{nameof(MakeCoffeeConsumer)}: Starting to make coffee ({context.Message.CoffeeType}) for order id = {context.Message.OrderId}.");
+
+            await Task.Delay(3000);
+
+            _logger.LogInformation($"{nameof(MakeCoffeeConsumer)}: Completed to make coffee ({context.Message.CoffeeType}) for order id = {context.Message.OrderId}.");
+
+        }
+    }
+}
