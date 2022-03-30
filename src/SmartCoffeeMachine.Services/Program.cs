720281739d5c138bc90eca37ba35462c7b761243
Shawn Shi <ss.shawnshi@gmail.com>
2022-03-30 12:11:02 -0400

    Add template console app.

---
+++ src/SmartCoffeeMachine.Services/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SmartCoffeeMachine.Services
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
